1328a2b51c4a99c69bfafc83f5881bf3531aeea9
AmelieLockfat <amelie.lock-fat@etud.univ-jfc.fr>
2023-01-09 21:06:12 +0100

    méthode 2 faite 2

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import monprojet.dto.PopulationPays;
 import monprojet.entity.City;
 import monprojet.entity.Country;
 
@@ -17,6 +18,12 @@ public interface CountryRepository extends JpaRepository<Country, Integer> {
     + " WHERE c.country.id = :pid")
     public int populationparID(Integer pid);
 
+    @Query(value = "SELECT coun.name as nom, SUM(c.population) as pop "
+    + "FROM City c "
+    + "JOIN Country coun ON coun.id = c.country_id "
+    + "GROUP BY country_id" ,
+    nativeQuery = true)
+public List<PopulationPays> listePopulationPays();
 
 
 }
